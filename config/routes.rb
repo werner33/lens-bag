60b5db6be6a219e3be05149840bedc650802c133
Jordan <jordanmmanley@gmail.com>
2015-12-08 15:14:02 +0000

    experiment with lens page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+  get '/lenses' => 'static_pages#lenses'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
